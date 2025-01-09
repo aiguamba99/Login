65c76d2b688d16e51b9d3a73f165b4039272fab9
Alexi <Alexi@Alex>
2025-01-09 14:11:49 -0500

    Login Base de datos, video

---
+++ IntLoggin/src/main/java/com/mycompany/intloggin/IntLoggin.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.intloggin;
+
+import com.mycompany.intloggin.igu.Principal;
+
+public class IntLoggin {
+
+    public static void main(String[] args) {
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+    }
+}
